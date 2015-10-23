5da75c6d538837e39a61fd0e1975130c841e9b94
Eric Richter <richterscale123@yahoo.com>
2015-10-22 20:24:34 -0400

    Final formatting

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,6 @@
 ## used if it already exists and the matrix has not changed.
 
 ## This function creates a special matrix object that can cache its inverse
-
 makeCacheMatrix <- function(x = matrix()) {
     inv <- NULL
     set <- function(y) {
@@ -21,7 +20,6 @@ makeCacheMatrix <- function(x = matrix()) {
 ## This function computes the inverse of the matrix.
 ## If the inverse has already been calculated and the matrix
 ## has not changed then it retrieves the inverse from the cache.
-
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         inv <- x$getinv()
